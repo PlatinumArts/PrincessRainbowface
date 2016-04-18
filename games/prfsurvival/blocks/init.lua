9a5b361e3a819b11f20e3f3db6377c5966a08ad9
Miguel Isaac <mikik@hotmail.es>
2016-04-17 19:26:47 -0500

    Create init.lua

---
+++ games/prfsurvival/blocks/init.lua
@@ -0,0 +1,11 @@
+print("Princess Rainbow Face Data Loading")
+
+--Princess Rainbow Face
+--Idea by: TheRiperKing(Mike T)
+--Code by: MineYoshi(Miguel Isaac Higuera)
+--To load the code!
+dofile(minetest.get_modpath("blocks").."/blocks_a.lua"
+dofile(minetest.get_modpath("blocks").."/blocks_b.lua"
+dofile(minetest.get_modpath("blocks").."/blocks_c.lua"
+
+print("Data has ended of load")
