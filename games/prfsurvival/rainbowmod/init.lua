bd71153a77a514f2e4927141cf8a5062ef9e6ebd
Miguel Isaac <mikik@hotmail.es>
2016-04-18 17:53:24 -0500

    Update init.lua

--- games/prfsurvival/rainbowmod/init.lua
+++ games/prfsurvival/rainbowmod/init.lua
@@ -13,6 +13,6 @@ print("Data is loading, wait a moment please...")
 dofile(minetest.get_modpath("rainbowmod").."/blocks.lua")
 dofile(minetest.get_modpath("rainbowmod").."/mapgen.lua")
 dofile(minetest.get_modpath("rainbowmod").."/tools.lua")
---dofile(minetest.get_modpath("rainbowmod").."/better_graphics.lua")  --Please Someone add this feature before end with the principal thing!
+--dofile(minetest.get_modpath("rainbowmod").."/better_graphics.lua")  --Please Someone add this feature after end with the principal thing!
 
 print("Data Finished Loading, Enjoy PrincessRainbowFace Survival Mode Game")
