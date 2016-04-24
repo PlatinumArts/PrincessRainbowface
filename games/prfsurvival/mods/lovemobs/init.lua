ac3e8819ad933ba0ab2d72c05b81a312339de1ca
Miguel Isaac <mikik@hotmail.es>
2016-04-24 15:01:19 -0500

    enemy_a included!

--- games/prfsurvival/mods/lovemobs/init.lua
+++ games/prfsurvival/mods/lovemobs/init.lua
@@ -4,7 +4,9 @@
 
 
 dofile(minetest.get_modpath("lovemobs").."/api.lua");
-dofile(minetest.get_modpath("lovemobs").."/friendly_person.lua")
+dofile(minetest.get_modpath("lovemobs").."/friendly_person.lua");
+dofile(minetest.get_modpath("lovemobs").."/enemy_a.lua")
 
 ---Fix this!
 ---(this code need work!)
+----Things TODO: add more enemies!
