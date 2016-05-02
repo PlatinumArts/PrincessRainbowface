943263851f4740e6f5528b29d3ec2aeac18a06b1
Miguel Isaac <mikik@hotmail.es>
2016-05-01 21:47:00 -0500

    Update init.lua

--- games/prfsurvival/mods/more_stuff/init.lua
+++ games/prfsurvival/mods/more_stuff/init.lua
@@ -1,6 +1,6 @@
 ---"more_stuff" this mod adds good content to prf---
 
 
-dofile(minetest.get_modpath("lovemobs").."/tetris.lua")
-
+--dofile(minetest.get_modpath("lovemobs").."/tetris.lua")
+--Under Coding!
 ---TODO: more arcades!---
