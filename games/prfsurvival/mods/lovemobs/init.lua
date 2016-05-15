230cf6e1a791a644e74bf5823f4c366ab6dbdc64
Miguel Isaac <mikik@hotmail.es>
2016-05-15 13:41:22 -0500

    Update init.lua

--- games/prfsurvival/mods/lovemobs/init.lua
+++ games/prfsurvival/mods/lovemobs/init.lua
@@ -5,7 +5,7 @@
 
 dofile(minetest.get_modpath("lovemobs").."/api.lua");
 dofile(minetest.get_modpath("lovemobs").."/friendly_person.lua");
-dofile(minetest.get_modpath("lovemobs").."/enemy_a.lua")
+--dofile(minetest.get_modpath("lovemobs").."/enemy_a.lua")
 
 ---Fix this!
 ---(this code need work!)
