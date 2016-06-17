d2181171699c7674fc0548258e46a52115f6649a
Miguel Isaac <mikik@hotmail.es>
2016-06-16 19:35:16 -0500

    Create nodes.lua

---
+++ games/creative_node/mods/default/nodes.lua
@@ -0,0 +1,35 @@
+---Nodes.lua
+
+
+
+minetest.register_node("default:grass", {
+	description = "Grass",
+	tiles = {"grass.png"},
+	groups = {crumbly=3}
+})
+
+minetest.register_node("default:dirt", {
+	description = "Dirt",
+	tiles = {"dirt.png"},
+	groups = {crumbly=3}
+})
+
+minetest.register_node("default:sand", {
+	description = "Sand",
+	tiles = {"sand.png"},
+	groups = {crumbly=3}
+})
+
+minetest.register_node("default:stone", {
+	description = "Grass",
+	tiles = {"grass.png"},
+	groups = {cracky=1}
+})
+
+minetest.register_node("default:alphnium", {
+	description = "Alphnium Ore",
+	tiles = {"alphnium.png"},
+	groups = {cracky=2}
+})
+
+
