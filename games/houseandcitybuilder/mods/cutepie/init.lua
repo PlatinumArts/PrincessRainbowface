a2176219f423712dc4d37e0f863f0c1a02d1b339
Mike <no>
2016-08-08 21:38:38 -0400

    Love bombs removed.  MtFoods added

--- games/houseandcitybuilder/mods/cutepie/init.lua
+++ games/houseandcitybuilder/mods/cutepie/init.lua
@@ -2,7 +2,7 @@ dofile(minetest.get_modpath("cutepie").."/cuteblock.lua")
 dofile(minetest.get_modpath("cutepie").."/blocks.lua")
 dofile(minetest.get_modpath("cutepie").."/crazyblocks.lua")
 dofile(minetest.get_modpath("cutepie").."/crafts.lua")
-dofile(minetest.get_modpath("cutepie").."/lovebomb.lua")
+--dofile(minetest.get_modpath("cutepie").."/lovebomb.lua")
 
 
 
